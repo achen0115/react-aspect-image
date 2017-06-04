383a31b14a48f6f565696602448ad55352b2e8b4
Rock Hu <rockia@mac.com>
2017-06-04 00:44:24 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# react-aspect-image
+
+A ReactJS image component to dynamically display image in original image ratio.
+
+## Installation
+
+```
+npm install react-aspect-image --save
+
+or
+
+yarn add react-aspect-image
+```
+
+
+## Usage
+
+You can use AspectImage almost the same way as a <img> and it will handle the image size for you.
+
+```
+import AspectImage from 'react-aspect-image';
+...
+<AspectImage
+  src="image_url"
+  fallback="image_url" //If your image failed to load, it will display this image, optional
+  alt="awesome image"
+  dimention={{height: 100, width:100}}  // specify your image dimention
+/>
+...
+```
+
+
+
+### Notes
+
+This is my first ever open source project. Please correct me if you see any mistakes (should be easy to find). :)
+
+## License
+
+MIT License
+
+Copyright (c) 2017 Rock Hu.
+
---
+++ src/index.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import noImage from './img/no_image.png';
+
+class AspectImage extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            loading: true,
+            src: this.props.src,
+            mode: this.props.mode,
+            height: this.props.dimention.height,
+            width: this.props.dimention.width
+        }
+    }
+
+    // Return default image if there is no default image props passed in
+    getFallbackImage = () => {
+        return this.props.fallback || noImage;
+    }
+
+  // Return different size for <img> acoording to different mode
+    getDimentionByMode = (mode,imageSize) => {
+        switch (mode) {
+        case 'aspect':
+            const base = Math.max(imageSize.height, imageSize.width);
+            return ({
+                width: this.state.width * (imageSize.width / base),
+                height: this.state.height * (imageSize.height / base)
+            });
+
+        default:
+            break;
+        }
+    }
+
+    imageLoaded = ({target}) =>{
+        const imageSize= {width: target.offsetWidth, height: target.offsetHeight};
+        console.log(imageSize);
+        const size = this.getDimentionByMode(this.state.mode, imageSize);
+        this.setState({
+            loading: false,
+            width: size.width,
+            height: size.height
+        });
+    }
+
+    imageError = () => {
+        this.setState({
+            loading:false,
+            src:this.getFallbackImage()
+        });
+    }
+
+    render() {
+        return (
+            <img 
+                src={this.state.src} 
+                alt={this.props.alt}
+                onLoad={this.imageLoaded}
+                onError={this.imageError}
+                height={this.state.loading ? null :this.state.height}
+                width={this.state.loading? null : this.state.width}
+            />
+        );
+    }
+}
+
+AspectImage.propTypes = {
+    src: PropTypes.string.isRequired,
+    dimention: PropTypes.object,
+    mode: PropTypes.string
+}
+
+AspectImage.defaultProps = {
+    dimention: {
+        width: 100,
+        height: 100
+    },
+    mode: 'aspect'
+}
+
+export default AspectImage;
